dc7da1c6a897da43c67d118eca94db3251015b5c
Adi Bal-Mayel <abalmayel@gmail.com>
2025-02-06 14:06:01 -0500

    Add options

--- project-5/app.py
+++ project-5/app.py
@@ -32,3 +32,39 @@ def index():
 def car(car_id):
     car = get_car(car_id)
     return render_template('car.html', car=car)
+
+
+@app.route('/<int:id>/delete', methods=('POST',))
+def delete(id):
+    post = get_car(id)
+    conn = get_db_connection()
+    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
+    conn.commit()
+    conn.close()
+    flash('"{}" was successfully deleted!'.format(car['brand']))
+    return redirect(url_for('index'))
+
+@app.route('/<int:id>/edit', methods=('GET', 'POST'))
+def edit(id):
+    post = get_car(id)
+
+    if request.method == 'POST':
+        brand = request.form['brand']
+        model = request.form['model']
+        m_year = request.form['m_year']
+        color = request.form['color']
+        province = request.form['province']
+        city = request.form['city']
+
+        if not brand:
+            flash('Brand is required!')
+        else:
+            conn = get_db_connection()
+            conn.execute('UPDATE cars SET brand = ?, model = ?, m_year = ?, color = ?, province = ?, city = ?'
+                         ' WHERE id = ?',
+                         (brand, model, m_year, color, province, city, id))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+
+    return render_template('edit.html', post=post)
